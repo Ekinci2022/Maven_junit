60d7fe71178105e9e4d4812cd73a7066e484be85
Abdullah Ekinci <abdullahhanekinci@gmail.com>
2023-01-06 21:57:18 +0100

    v1

--- src/test/java/utilities/TestBase.java
+++ src/test/java/utilities/TestBase.java
@@ -7,43 +7,63 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
 
 public abstract class TestBase {
-    //    driver objesini olustur. Driver ya public yada protected olmali. Sebepi child classlarda gorulebilir olmasi
+    //    TestBase i abstract yapmamizin sebebi bu sinifin objesini olusturmak istemiyorum
+//    TestBase testBase = new TestBase(); -> YAPILAMAZ
+//    Amacim bu sinifi extend etmek ve icindeki hazir metodlari kullanmak
+//    driver objesini olustur. Driver ya public yada protected olmali.
+//    Sebepi child classlarda gorulebilir olmasi
     protected static WebDriver driver;
-
     //    setUp
     @Before
-    public void setup() {
+    public void setup(){
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
     }
+    //    tearDown
+    @After
+    public void tearDown(){
+        waitFor(5);
+      driver.quit();
+    }
+    //    MULTIPLE WINDOW:
+//    1 parametre alir : Gecis Yapmak Istedigim sayfanin Title
+//    ORNEK:
+//    driver.get("https://the-internet.herokuapp.com/windows");
+//    switchToWindow("New Window");
+//    switchToWindow("The Internet")
+    public static void switchToWindow(String targetTitle) {
+        String origin = driver.getWindowHandle();
+        for (String handle : driver.getWindowHandles()) {
+            driver.switchTo().window(handle);
+            if (driver.getTitle().equals(targetTitle)) {
+                return;//CIK. break;
+            }
+        }
+        driver.switchTo().window(origin);
+    }
+    //    windowNumber sıfır (0)'dan başlıyor.
+//    index numarasini parametre olarak alir
+//    ve o indexli pencerece gecis yapar
+    public static void switchToWindow(int windowNumber){
+        List<String> list = new ArrayList<>(driver.getWindowHandles());
+        driver.switchTo().window(list.get(windowNumber));
+    }
 
-    //tearDown
 
-//    @After
-//    public void tearDown() {
-//        driver.quit();
-//    }
-
-    //    MULTIPLE WINDOW
-// 1 parametre alin ; Gecis Yapmak istedigim şqyfanin Title
-    //ornek :
-    //driver.get("https://the-internet.herokuapp.com/windows");
-    //switchToWindow("New Window")
-    //switchToWindow("The internet")
-
-//    public static void switchToWindow(String targetTitle) {
-//        String origin = driver.getWindowHandle();
-//        for (String handle : driver.getWindowHandles()) {
-//            driver.switchTo().window(handle);
-//            if (driver.getTitle().equals(targetTitle)) {
-//                return;
-//            }
-//        }
-//        driver.switchTo().window(origin);
-
-
+    /*   HARD WAIT:
+  @param : second
+*/
+    public static void waitFor(int seconds){
+        try {
+            Thread.sleep(seconds*1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
